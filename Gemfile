fdc7e52e93c68709fdaf3e1584684c9433440229
Anthony Corrado <amcstudios@yahoo.com>
2014-07-02 02:44:44 -0700

    styling map index

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'yelp'
 gem 'oauth'
 
 gem 'foundation-rails'
+gem 'foundation_rails_helper'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.0'
 # Use SCSS for stylesheets
@@ -23,7 +24,6 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -9,5 +9,32 @@ class BarsController < ApplicationController
       end
 		end
 
+		def show
+		@bar = Bar.find(params[:id])
+	end
+
+	def new
+		@bar = Bar.new
+	end
+
+	def create
+		Bar.create(params.require(:bar).permit(:biz_id, :name, :street, :city_state_zip, :day, :time, :theme, :web, :lat, :lon)) if params[:status]
+		redirect_to bars_path(@bar)
+	end
+
+	def edit
+		@bar = Bar.find(params[:id])
+	end
+
+	def update
+		@bar = Bar.find(params[:id])
+		if @bar.update(params.require(:bar).permit(:biz_id, :name, :street, :city_state_zip, :day, :time, :theme, :web, :lat, :lon))
+			redirect_to bars_path
+		else
+			render 'edit'
+		end
+	end
+
+
 
 end
